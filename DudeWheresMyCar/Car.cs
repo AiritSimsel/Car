73671deb39bf87cb33c8704ee8b6dbb583c3434f
Airit Simsel <airitkarm@gmail.com>
2021-02-25 17:00:53 +0200

    Dude Where's my Car

---
+++ DudeWheresMyCar/Car.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DudeWheresMyCar
+{
+    class Car
+    {
+        
+        
+        //The car can drive.Every lap the car passes increases the value of the odometer by 100 and decreases the value of the fuel tank by 5 .
+
+        public string mark;
+        public string model;
+        public string registrationNumber;
+        public string colour;
+        public int odometer = 0;
+        public int fuelTank = 60;
+
+        public Car(string _mark, string _model, string _registrationNumber, string _colour)
+        {
+            mark = _mark;
+            model = _model;
+            registrationNumber = _registrationNumber;
+            colour = _colour;
+            
+        }
+
+        public void ViewCar()
+        {
+            Console.WriteLine($"Mark: {mark}\nModel: {model}\nRegistration number: {registrationNumber}\nColour: {colour}\nOdometer count: {odometer}\nFuel tank: {fuelTank}");
+        }
+
+        public void Lap()
+        {
+            odometer += 100;
+            fuelTank -= 5;
+            if (fuelTank == 0)
+            {
+                Console.WriteLine("The fueltank is empty!");
+            }
+            
+        }
+
+
+
+
+    }
+}
---
+++ DudeWheresMyCar/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DudeWheresMyCar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car auto = new Car("VW", "Golf", "895BLS", "blue");
+            auto.ViewCar();
+            auto.Lap();      
+            auto.ViewCar();
+
+            while (auto.fuelTank > 0)
+            
+            {
+                auto.Lap();
+            }
+            auto.ViewCar();
+        }
+    }
+}
